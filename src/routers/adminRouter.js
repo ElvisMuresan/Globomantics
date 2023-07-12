830661ad564343305c90024a93fd48af10d202b3
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-07-12 23:56:44 +0300

    Solved the problem with connection to database with help from Vlad

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -6,18 +6,16 @@ const sessions = require("../data/sessions.json");
 const adminRouter = express.Router();
 
 adminRouter.route("/").get((req, res) => {
+  debug("/admin has been hit");
   const url =
-    "mongodb+srv://dbUser:Barcelona3!@!@globomantics.f60mfj4.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://dbUser:Barcelona3!%40!@globomantics.f60mfj4.mongodb.net/?retryWrites=true&w=majority";
   const dbName = "globomantics";
-
   (async function mongo() {
     let client;
     try {
       client = await MongoClient.connect(url);
       debug("Connected to the mongo DB");
-
       const db = client.db(dbName);
-
       const response = await db.collection("sessions").insertMany(sessions);
       res.json(response);
     } catch (error) {
